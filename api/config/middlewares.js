08329fa47818393893eceb4063de28f79a5a16e5
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-04 20:49:15 +0300

    Update middlewares.js
    render

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,7 +1,14 @@
 module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://deco-shop-front.onrender.com/'], 
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
