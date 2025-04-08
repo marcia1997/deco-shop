b65e59189a6c41cc3c21fee08db7ea5418c64ad2
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-08 22:45:50 +0300

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -2,12 +2,15 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-    
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'password'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), 
+      },
     },
+    debug: false,
   },
 });
