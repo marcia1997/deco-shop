9dc1796754a79cc3ee862a491f37307aaeaf19bf
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-08 23:26:35 +0300

    Update server.js

--- api/config/server.js
+++ api/config/server.js
@@ -1,13 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env('PUBLIC_URL', 'https://deco-shop.onrender.com'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
-    url: '/admin',
-  },
 });
-
