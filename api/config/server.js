ee5eacb83afd3782e2267bfeeb721949f92bbde5
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-08 23:33:11 +0300

    Update server.js

--- api/config/server.js
+++ api/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 'https://deco-shop.onrender.com'),
+  port: env.int('PORT', 'https://deco-shop-front.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
