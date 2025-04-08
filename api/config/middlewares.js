d72e5735f82ed3ef32171a123e4a7c884f218c3a
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-08 16:18:20 +0300

    Update middlewares.js

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,11 +1,25 @@
-module.exports = ({ env }) => ({
-  settings: {
-    cors: {
-      enabled: true,
+module.exports = ({ env }) => [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors', // <- este es el que vamos a configurar
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
       origin: [
-        'https://deco-shop.onrender.com',
-        'http://localhost:3000',
+        'https://wecooking-front-e9qg.onrender.com', 
+        'http://localhost:3000',                   
+        'https://deco-shop.onrender.com',            
       ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: '*',
+      credentials: true,
     },
   },
-});
+];
