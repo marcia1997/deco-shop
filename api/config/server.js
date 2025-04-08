417cc6c692ef64462a670cbd6f2a4e655de8a0aa
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-08 12:38:53 +0300

    Update server.js

--- api/config/server.js
+++ api/config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    url: '/admin',
   },
 });
