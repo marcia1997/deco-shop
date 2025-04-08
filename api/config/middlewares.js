0739eca0b1dabd6e1552ed796fb3e4414ccf6f95
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-08 11:20:43 +0300

    update node

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = [
     config: {
       origin: ['https://deco-shop-front.onrender.com/'], 
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      headers: '*',
+      headers: ['Content-Type', 'Authorization'],
     },
   },
   'strapi::security',
