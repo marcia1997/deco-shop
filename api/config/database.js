b9e7c3d89ca7f6b8a9b6f5574c295ae31e13a1cd
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-08 21:57:01 +0300

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -7,9 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized: false,
-      },
+    
     },
   },
 });
