6d5bc81d2d753c9a6ba647d0667cd24dd2d2fc7f
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-09 13:35:14 +0300

    Update makeRequest.js

--- client/src/makeRequest.js
+++ client/src/makeRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL, // Base URL from your .env file
   headers: {
-    Authorization: "Bearer " + process.env.REACT_APP_API_TOKEN, 
+    "Content-Type": "application/json",
   },
 });
