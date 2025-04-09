21b3efcde6a93f50cb9540fb6fb8d7cca0923d9f
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-09 13:35:00 +0300

    Update FeaturedProducts.jsx

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -13,18 +13,19 @@ const FeaturedProducts = ({ type }) => {
       <div className="top">
         <h1>{type} products</h1>
         <p>
-        Each product in this collection embodies our commitment to quality, performance, and design excellence. Whether you're looking to upgrade your living space, enhance your personal style, or find the perfect gift for someone special, our Featured Products are here to inspire and delight.<br></br>
-
-       We believe that every item tells a story, and we're excited to share these stories with you. <br></br>
-
+          Each product in this collection embodies our commitment to quality, performance, and design excellence. Whether you're looking to upgrade your living space, enhance your personal style, or find the perfect gift for someone special, our Featured Products are here to inspire and delight.
+          <br />
+          We believe that every item tells a story, and we're excited to share these stories with you.
         </p>
       </div>
       <div className="bottom">
-        {error
-          ? "Something went wrong!"
-          : loading
-          ? "loading"
-          : data?.map((item) => <Card item={item} key={item.id} />)}
+        {error ? (
+          <div className="error">Error: {error}</div> // Display the actual error message
+        ) : loading ? (
+          <div className="loading">Loading...</div> // Show a loading state
+        ) : (
+          data?.map((item) => <Card item={item} key={item.id} />) // Display cards when data is loaded
+        )}
       </div>
     </div>
   );
