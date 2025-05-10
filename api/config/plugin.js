3f5b4f15643ab5386174e8117de5be70d4d79226
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-05-10 14:02:41 +0300

    Update plugin.js

--- api/config/plugin.js
+++ api/config/plugin.js
@@ -1,16 +1,13 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/cloudinary',
+      provider: 'cloudinary',
       providerOptions: {
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
+     
     },
   },
 });
